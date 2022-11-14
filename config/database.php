7e2f62dbe7e955c908cec33d5d579c76323f4f65
YUYONGXUE <yuyongxue@gmail.com>
2022-11-14 20:46:09 +0800

    1st Commit 2022-11-14

--- config/database.php
+++ config/database.php
@@ -1,6 +1,13 @@
 <?php
 
-use Illuminate\Support\Str;
+use Illuminate\Support\Str; 
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$host = $url["host"] ?? null;
+$username = $url["user"] ?? null;
+$password = $url["pass"] ?? null;
+$database = substr($url["path"], 1);
+
 
 return [
 
@@ -45,12 +52,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),  //env('DATABASE_URL')
-            'host' => 'us-cdbr-east-06.cleardb.net', //env('DB_HOST', '127.0.0.1')
+            //'url' => env("CLEARDB_DATABASE_URL"),               //env('DATABASE_URL')
+            'host' => $host,    // 'us-cdbr-east-06.cleardb.net',  //env('DB_HOST', '127.0.0.1')
             'port' => env('DB_PORT', '3306'),
-            'database' => 'heroku_bde3bd9298b9f68', //env('DB_DATABASE', 'forge')
-            'username' => 'be28bae2435b55', //env('DB_USERNAME', 'forge')
-            'password' => '62f7ef57', //env('DB_PASSWORD', '')
+            'database' => $database,    //'heroku_bde3bd9298b9f68',             //env('DB_DATABASE', 'forge')
+            'username' => $username,    //'be28bae2435b55',                     //env('DB_USERNAME', 'forge')
+            'password' => $password,    //'62f7ef57',                           //env('DB_PASSWORD', '')
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
