e750d8146e32831e34d5aa5429e2f6d96bb9f802
YuYX <yuyongxue@gmail.com>
2022-12-25 21:40:49 +0800

    2022-12-25:003

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -349,7 +349,7 @@
 
       <div class="col-sm-7 col-md-7 middle-col"  style="background-color:whitesmoke">   
             <div class="row mb-5" > 
-                <div class="card profile-image-container col-sm-2 col-md-2" 
+                <div class="card sticky-top profile-image-container col-sm-2 col-md-2" 
                     type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" 
                     style="background-image:url({{Storage::disk('s3')->url($profile->back_image)}}); 
                            border-style:none; border-radius: 10px;
@@ -372,7 +372,7 @@
                         alt=""> 
                       {{-- src="/storage/{{ $profile->image }}" alt="">    --}}
                 </div>
-                <div class="card col-sm-10 col-md-10"  
+                <div class="card sticky-top col-sm-10 col-md-10"  
                     style="background-color: rgb(250, 250, 246); border-radius:10px;">
                     <div class="card-body"> 
                         <a class="post-input-prompt nav-link animate__animated animate__bounceInRight " 
