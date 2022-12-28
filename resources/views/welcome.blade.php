198f0aac7c9193bd2dec51853cb8691501030517
YuYX <yuyongxue@gmail.com>
2022-12-28 21:00:00 +0800

    2022-12-28:004

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" 
-                integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" 
-                crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/animejs@3.0.1/lib/anime.min.js"></script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" 
+                integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" 
+                crossorigin="anonymous">
+        </script>
+
+        <script src="https://cdn.jsdelivr.net/npm/animejs@3.0.1/lib/anime.min.js"></script>
+
         <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
         />
-        <title>YUYONGXUE-Eureka!</title>
-        <link rel="icon" href="{{ url('images/archimedes.webp') }}">
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -29,7 +31,22 @@
             }
         </style> --}}
         
-        <script type="text/javascript" src="{{ asset('js/welcome.js')}}"></script> 
+        <script type="text/javascript" src="{{ asset('js/welcome.js')}}"></script>  
+        
+        <title>YUYONGXUE-Eureka!</title>
+        <link rel="icon" href="{{ url('images/archimedes.webp') }}">
+
+         <!-- css file -->
+         <link rel="stylesheet" type="text/css" href="{{ 'css/app.css' }}"> 
+         <link rel="stylesheet" type="text/css" href="{{ 'css/emojis.css' }}"> 
+         <link rel="stylesheet" type="text/css" href="{{ 'css/weather.css' }}">
+         <link rel="stylesheet" type="text/css" href="{{ 'css/radio.css' }}"> 
+         <link rel="stylesheet" type="text/css" href="{{ 'css/modal.css' }}"> 
+         <link rel="stylesheet" type="text/css" href="{{ 'css/profile.css' }}"> 
+     
+         <!-- Scripts -->
+         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
     </head> 
 
     {{-- <head>
