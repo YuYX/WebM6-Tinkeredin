629113d18464517af36fb72bd12f150c8b2432f4
YUYONGXUE <yuyongxue@gmail.com>
2023-01-25 22:17:24 +0800

    2023-01-25:004

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -210,7 +210,7 @@
               
                 <img class="rounded card-img-top mb-1" 
                 {{-- src="/{{ $profile->back_image }}"  --}}
-                src="{{ Storage::disk('s3')->url(optional($profile->back_image)) }}" 
+                src="{{ Storage::disk('s3')->url(optional($profile)->back_image) }}" 
                 alt="Profile background image not assigned"> 
                 {{-- src="/storage/{{ $profile->back_image }}" alt="">  --}}
                 <div class="mx-auto">
