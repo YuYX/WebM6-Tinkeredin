0ad4ee63fb9f819326188e7a586f8182848450d0
yuyongxue@gmail.com <yuyongxue@gmail.com>
2024-03-23 12:22:51 +0800

    1st commit on 2024-03-23

--- config/database.php
+++ config/database.php
@@ -51,35 +51,14 @@ return [
         ],
 
         // For Local Test
-        'mysql' => [
-            'driver' => 'mysql',
-            //'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-
-        // For Heroku Deployment - ClearDB
         // 'mysql' => [
         //     'driver' => 'mysql',
-        //     //'url' => env("CLEARDB_DATABASE_URL"),               //env('DATABASE_URL')
-        //     'host' => $host,  //env('DB_HOST', '127.0.0.1')
+        //     //'url' => env('DATABASE_URL'),
+        //     'host' => env('DB_HOST', '127.0.0.1'),
         //     'port' => env('DB_PORT', '3306'),
-        //     'database' => $database,    //'heroku_84ad0baa9817629',             //env('DB_DATABASE', 'forge')
-        //     'username' => $username,   //env('DB_USERNAME', 'forge')
-        //     'password' => $password,                               //env('DB_PASSWORD', '')
+        //     'database' => env('DB_DATABASE', 'forge'),
+        //     'username' => env('DB_USERNAME', 'forge'),
+        //     'password' => env('DB_PASSWORD', ''),
         //     'unix_socket' => env('DB_SOCKET', ''),
         //     'charset' => 'utf8mb4',
         //     'collation' => 'utf8mb4_unicode_ci',
@@ -91,6 +70,27 @@ return [
         //         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
         //     ]) : [],
         // ],
+
+        // For Heroku Deployment - ClearDB
+        'mysql' => [
+            'driver' => 'mysql',
+            //'url' => env("CLEARDB_DATABASE_URL"),               //env('DATABASE_URL')
+            'host' => $host,  //env('DB_HOST', '127.0.0.1')
+            'port' => env('DB_PORT', '3306'),
+            'database' => $database,    //'heroku_84ad0baa9817629',             //env('DB_DATABASE', 'forge')
+            'username' => $username,   //env('DB_USERNAME', 'forge')
+            'password' => $password,                               //env('DB_PASSWORD', '')
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
                 // For Heroku Deployment - RDS
                 // 'mysql' => [
                 //     'driver' => 'mysql',
@@ -112,6 +112,7 @@ return [
                 //     ]) : [],
                 // ],
 
+        //PostgreSQL
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
